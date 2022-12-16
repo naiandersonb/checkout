6fc9dc9b76b364c12fb911efd55c083db02e7fcc
naiandersonb <nayanderson2014@gmail.com>
2022-12-16 14:35:49 -0300

    script

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const buttonUpsell = document.getElementById('tm-upsell');
 const buttonCancelUpsell = document.getElementById('tm-upsell-cancel');
 const tokenUrl = window.location.href;
+const params = new URLSearchParams(window.location.search);
 
 
 buttonUpsell.addEventListener('click', async () => {
-  const offer_id = buttonUpsell.dataset.off;
+  const offer_id = buttonUpsell.dataset.offer;
   const product_id = buttonUpsell.dataset.prod;
+  const user_id = params.get("user_id");
+  const customer_id = params.get("customer_id");
+  // const token = params.get("token");
 
-  const urlSplit = tokenUrl.split('?token=');
-  const token = urlSplit[1];
-
-  const data = {
+  const data = JSON.stringify({
     offer_id,
+    user_id,
+    customer_id,
     product_id,
-    token
-  }
+  })
 
-  await fetch('/link/post', {
+  // alert(JSON.stringify(data, null, 2))
+  await fetch('https://v1.themembers.dev.br/api/checkout/upsell', {
     method: 'POST',
     body: data
   })
-    .then(res => console.log({ res }))
+    .then(res => res.json()).then(res => console.log({ res }))
     .catch(err => console.log({ err }));
-
 })
 
 buttonCancelUpsell.addEventListener('click', () => {
   console.log('cancel upsell');
-})
+});
